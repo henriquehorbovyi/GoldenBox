26727aa930369625eba4669ebd651e1546c902f0
Carlos Henrique <carloshenry.wm@gmail.com>
2019-02-20 06:29:01 -0300

    Created some modules and implemented android navigation.

--- app/src/main/java/com/henry/goldenbox/ui/MainActivity.kt
+++ app/src/main/java/com/henry/goldenbox/ui/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.henry.goldenbox.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +21,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupNavigation(){
         navController = findNavController(R.id.nav_host_fragment)
+        bottom_navigation_view.setupWithNavController(navController)
     }
 }
